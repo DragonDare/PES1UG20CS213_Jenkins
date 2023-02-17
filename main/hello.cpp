61c861a4b92f0997a69ada39e781455a0e57e713
DragonDare <dragondare@pop-os.localdomain>
2023-02-18 01:08:30 +0530

    first commit

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World from PES1UG20CS213\n";
+  cout << "Hello, Jenkins:D\n";
+  cout << "I have successfully built and run my life, not\n";
+  return 0;
+}
