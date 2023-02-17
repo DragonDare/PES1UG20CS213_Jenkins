7c48f2ade5251afc39e16e8cfef26511f4c2c94f
Kohav Dey <deykohav@gmail.com>
2023-02-18 02:48:51 +0530

    Delete cs213_5.cpp

--- cs213_5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "CS213 from Task 5, copy? Over";
-  cout << "Hello there 213, its been long
-  cout << "Long since i have kicked your ass, yes";
-  return 0;
-}
